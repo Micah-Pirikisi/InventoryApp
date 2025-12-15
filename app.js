70c201507b247c172f27938ded7978554f2f2062
Micah-Pirkisi <micahpirikisi@gmail.com>
2025-12-15 11:02:09 +0200

    Hook last routes into app.js

--- app.js
+++ app.js
@@ -11,16 +11,20 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 
 // Import route files
-const categoryRoutes = require('./routes/categoryRoutes');
-const filmRoutes = require('./routes/filmRoutes');
-const actorRoutes = require('./routes/actorRoutes');
-const directorRoutes = require('./routes/directorRoutes');
+const categoryRoutes = require("./routes/categoryRoutes");
+const filmRoutes = require("./routes/filmRoutes");
+const actorRoutes = require("./routes/actorRoutes");
+const directorRoutes = require("./routes/directorRoutes");
+const filmActorRoutes = require("./routes/filmActorRoutes");
+const filmDirectorRoutes = require("./routes/filmDirectorRoutes");
 
 // Use them with prefixes
-app.use('/categories', categoryRoutes);
-app.use('/films', filmRoutes);
-app.use('/actors', actorRoutes);
-app.use('/directors', directorRoutes);
+app.use("/categories", categoryRoutes);
+app.use("/films", filmRoutes);
+app.use("/actors", actorRoutes);
+app.use("/directors", directorRoutes);
+app.use("/films", filmActorRoutes);
+app.use("/films", filmDirectorRoutes);
 
 // Start Server
 app.listen(8000, () => {
