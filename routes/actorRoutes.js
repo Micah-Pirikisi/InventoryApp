44e9512e6b4cfb7299e1cd4cb637892ef299780f
Micah-Pirkisi <micahpirikisi@gmail.com>
2025-12-15 10:10:31 +0200

    Map actorContro ller functions to URLs in actorRoutes.js

---
+++ routes/actorRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const actorController = require("../controllers/actorController");
+
+// CRUD routes
+router.get("/", actorController.getAllActors);
+router.get("/:id", actorController.getActorById);
+router.post("/", actorController.createActor);
+router.put("/:id", actorController.updateActor);
+router.delete("/:id", actorController.deleteActor);
+
+module.exports = router;
