5b9a373bc5f171d171d4a5abc248d2c0cd84e510
Micah-Pirkisi <micahpirikisi@gmail.com>
2025-12-17 07:48:39 +0200

    Add new and edit routes to routes/actorRoutes.js

--- routes/actorRoutes.js
+++ routes/actorRoutes.js
@@ -2,6 +2,28 @@ const express = require("express");
 const router = express.Router();
 const actorController = require("../controllers/actorController");
 
+// Render new actor form
+router.get("/new", (req, res) => {
+  res.render("actors/new");
+});
+
+// Render edit actor form (load actor first)
+router.get("/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await require("../db").query(
+      "SELECT * FROM actors WHERE actor_id = $1",
+      [id]
+    );
+    if (result.rows.length === 0)
+      return res.status(404).send("Actor not found");
+    res.render("actors/edit", { actor: result.rows[0] });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
+});
+
 // CRUD routes
 router.get("/", actorController.getAllActors);
 router.get("/:id", actorController.getActorById);
