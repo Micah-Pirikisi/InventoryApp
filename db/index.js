7a93873bf476c5274dc0f01fa3adee0e59ba06fe
Micah-Pirkisi <micahpirikisi@gmail.com>
2025-12-17 11:59:05 +0200

    Prep for koyeb and aiven

--- db/index.js
+++ db/index.js
@@ -1,12 +1,12 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: 'devmicah',
-  host: 'localhost',
-  database: 'devmicah',
-  password: 'micahtadiwapirikisi', // the password you set
-  port: 5432,
+  host: process.env.PGHOST,        // e.g. pg-1924eeae-expressdb.f.aivencloud.com
+  port: process.env.PGPORT,        // e.g. 20101
+  database: process.env.PGDATABASE, // e.g. defaultdb
+  user: process.env.PGUSER,        // e.g. avnadmin
+  password: process.env.PGPASSWORD, // your Aiven password
+  ssl: { rejectUnauthorized: false } // Aiven requires SSL
 });
 
 module.exports = pool;
-
